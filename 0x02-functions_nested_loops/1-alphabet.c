3265f28b304586e84f724eacf03d78cf82057f1b
Yonathan-dotcom <y.geda@yahoo.com>
2022-09-13 17:33:38 +0000

    print alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -5,8 +5,12 @@
  *
  * Return: Always 0.
  */
-int main(void)
+void print_alphabet(void)
 {
-	print_alphabet();
-	return(0);
+	char a;
+	for (a = 'a'; a <= 'z'; a++)
+	{
+		_putchar(a);
+	}
+_putchar('\n');
 }
