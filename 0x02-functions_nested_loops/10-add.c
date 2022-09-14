0c223064fd13f16afdbf56c305e6f44813163291
Yonathan-dotcom <y.geda@yahoo.com>
2022-09-14 08:01:51 +0000

    print a function that adds two numbers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * add - Check code
+ * @a: An intiger a
+ * @b: An intiger b
+ * Description: Function that adds two intigers and returns the result
+ * Return: Result to add a and b
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+
