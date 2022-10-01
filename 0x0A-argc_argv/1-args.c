f06b6cb4aeabfac2553cf3c8ff5d908a35efff28
Yonathan-dotcom <y.geda@yahoo.com>
2022-10-01 07:32:58 +0000

    number of arguements passed to it

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,10 +1,11 @@
-#include"main.h"
+#include "main.h"
 #include <stdio.h>
+
 /**
- *
- *
- *
- *
+ * main - A program that prints the number of arguements you passed into it
+ * @argc: An argument counter
+ * @argv: An argument values
+ * Return: Always 0 (Success)
  */
 int main(int argc, char *argv[])
 {
@@ -12,3 +13,4 @@ int main(int argc, char *argv[])
 		printf("%d\n", argc - 1);
 	return (0);
 }
+
