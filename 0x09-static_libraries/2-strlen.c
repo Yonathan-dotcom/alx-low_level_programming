8c1fc64c476d464e3b716984e01d008090a21d28
Yonathan-dotcom <y.geda@yahoo.com>
2022-10-02 18:35:22 +0000

    task 0

---
+++ 0x09-static_libraries/2-strlen.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _strlen -function returns the lengsth of a string.
+ * *s - is an input string.
+ * Return: Nohthing.
+ */
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (s[len] != '\0')
+		len++;
+	return (len);
+}
